67344ade7d7cc4611e6e102d5e7d737bf346491a
Duy Nguyen <h.nguyen@se17.qmul.ac.uk>
2020-10-21 21:29:35 +0100

    Movement is added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,31 +5,111 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 
 {
-    public float moveSpeed;
+    //Player movement and rigidbody 2d
+    public Rigidbody2D rb;
+    public Vector2 movement;
+    //----------------------------Checking speed and jump height
+    public float speed;
     public float jumpHeight;
+    //Check facing direction
+    private bool facingRight;
+    //------------------------------------Check for ground
+    public bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask whatIsGround;
+    //----------------------------------JUMPING
+    public int extraJumps;
+    public int extraJumpsValue;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        jumpHeight = 5;
-        moveSpeed = 4;
-
+        extraJumpsValue = 2;
+        speed = 20.0f;
+        jumpHeight = 5.0f;
+        facingRight = true;
+        rb = GetComponent<Rigidbody2D>();
     }
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        //check the input 
+        checkMovementInput();
+        //check facing direction
+        checkFacingDirection();
+    }
+    void FixedUpdate()
+    {
+        //Check if player is grounded
+        checkGround();
+        //Move the player
+        movePlayer(movement);
+    }
+    void checkGround()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, whatIsGround);
+    }
+    void checkFacingDirection()
+    {
+        if (movement.x > 0 && facingRight == false)
         {
-            GetComponent<Rigidbody2D>().velocity = (new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight));
+            Flip();
+        }
+        else if (movement.x < 0 && facingRight == true)
+        {
+            Flip();
+        }
+    }
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+
+    }
+    //------------------------------MOVEMENT METHODS---------------------------------
+    void checkMovementInput()
+    {
+        if (isGrounded)
+        {
+            extraJumps = extraJumpsValue;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            GetComponent<Rigidbody2D>().velocity = (new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y));
+            movement = (new Vector2(speed, 0));
         }
-        if (Input.GetKey(KeyCode.A))
+        else if (Input.GetKey(KeyCode.A))
         {
-            GetComponent<Rigidbody2D>().velocity = (new Vector2(-(moveSpeed), GetComponent<Rigidbody2D>().velocity.y));
+            movement = (new Vector2(-speed, 0));
         }
+        else if (Input.GetKeyDown(KeyCode.Space) && extraJumps != 0)
+        {
+            movement = (new Vector2(0, jumpHeight));
+            extraJumps--;
+        }
+        else if (Input.GetKeyDown(KeyCode.Space) && extraJumps == 0 && isGrounded)
+        {
+            movement = (new Vector2(0, jumpHeight));
+        }
+        else
+        {
+            movement = (new Vector2(0, 0));
+        }
+    }
+    void movePlayer(Vector2 direction)
+    {
+        if (direction.y <= 0)
+        {
+            rb.AddForce(direction);
 
-
+        }
+        else
+        {
+            rb.AddForce(direction, ForceMode2D.Impulse);
+        }
     }
 }
