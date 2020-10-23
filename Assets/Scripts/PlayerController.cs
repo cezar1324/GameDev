1be74a1c00770424147a3be170b517b6a23adfd6
Duy Nguyen <h.nguyen@se17.qmul.ac.uk>
2020-10-23 17:52:22 +0100

    dsaasdasd

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -29,7 +29,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        extraJumpsValue = 5;
+        extraJumpsValue = 12;
         speed = 10.0f;
         jumpHeight = 7.0f;
         facingRight = true;
